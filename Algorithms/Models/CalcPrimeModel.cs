00cd76defb8a640abb32dea5bc2ee072552b17c6
MarcusChang7 <marcuschang@marcuschang.com>
2016-07-05 21:03:56 -0400

    CalcPrimeModel

---
+++ Algorithms/Models/CalcPrimeModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace Algorithms.Models
+{
+  public  class CalcPrimeModel
+    {
+      [DisplayName("Number to Check")]
+        public int Numb2Calc { get; set; }
+        public bool Answer { get; set; }
+    }
+}
